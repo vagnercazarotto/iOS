6c963bbfcfbeed0ffe7579e37af0c79e40f30414
vagnercazarotto <vagnercazarotto@gmail.com>
2016-07-03 21:31:16 -0300

    testing git .ignore

---
+++ basics/MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var x: Int = 40
+let y = 100
+
+
+var myString = "Hello"
+
+
+if x < 50{
+    print("X is less than 50")
+}else{
+    print("X is greater than or equal to 50")
+}
+
+
+class ViewController: NSOutlineView{
+    
+}
+
+
+func printHello(){
+    print("Hello")
+}
+
+
+printHello()
