6177da7701625b3f7ebe169d44bb0d3caa53b4f3
vagnercazarotto <vagnercazarotto@gmail.com>
2016-07-06 10:19:37 -0300

    Adding Delegation to RecordSounds

--- basics/PitchPerfect/PitchPerfect/recordSoundsViewController.swift
+++ basics/PitchPerfect/PitchPerfect/recordSoundsViewController.swift
@@ -9,13 +9,14 @@
 import UIKit
 import AVFoundation
 
-class recordSoundsViewController: UIViewController {
+class recordSoundsViewController: UIViewController , AVAudioRecorderDelegate {
     
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
     
     var audioRecorder:AVAudioRecorder!
+    
     
 
     override func viewDidLoad() {
@@ -45,6 +46,7 @@ class recordSoundsViewController: UIViewController {
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
         
         try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.delegate = self
         audioRecorder.meteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
@@ -69,6 +71,25 @@ class recordSoundsViewController: UIViewController {
         print("viewWillAppear called")
     }
     
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        print("AVAudioRecorder finished saving recording")
+        if (flag) {
+            self.performSegueWithIdentifier("stopRecording", sender: audioRecorder.url)
+        } else {
+            print("Saving of recording failed")
+        }
+        
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "stopRecording"){
+            let playSoundsVC = segue.destinationViewController as! PlaySoundsViewController
+            let recordedAudioURL = sender as! NSURL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
     
 }
 
