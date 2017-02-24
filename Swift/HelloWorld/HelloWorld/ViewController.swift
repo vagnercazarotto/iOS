721394be26f12282071730b27d32ae07252d9e1b
vagnercazarotto <vagnercazarotto@gmail.com>
2017-02-24 14:56:51 -0300

    the first problem

--- Swift/HelloWorld/HelloWorld/ViewController.swift
+++ Swift/HelloWorld/HelloWorld/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    
+    @IBOutlet weak var titleImage: UIImageView!
+    
+    @IBOutlet weak var welcomeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    
+    
+    @IBAction func welcomePress(_ sender: Any) {
+        background.isHidden = false
+        titleImage.isHidden = false
+        welcomeButton.isHidden = true
+        
+    }
+    
+    
 
 }
 
