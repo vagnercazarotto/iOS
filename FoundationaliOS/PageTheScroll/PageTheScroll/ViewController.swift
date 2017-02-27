f811ae365ae62e6602392cc7da715716b03d59ac
vagnercazarotto <vagnercazarotto@gmail.com>
2017-02-27 14:42:15 -0300

    Initial commit for PageTheScroll

---
+++ FoundationaliOS/PageTheScroll/PageTheScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageTheScroll
+//
+//  Created by Vagner Cazarotto on 2/27/17.
+//  Copyright © 2017 Vagner Cazarotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
