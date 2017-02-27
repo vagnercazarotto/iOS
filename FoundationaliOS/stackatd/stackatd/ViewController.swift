600e24965f6445bee88e123e2fa245fde1f7a81d
vagnercazarotto <vagnercazarotto@gmail.com>
2017-02-27 20:10:30 -0300

    Initial commit for stack project

---
+++ FoundationaliOS/stackatd/stackatd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackatd
+//
+//  Created by Vagner Cazarotto on 2/27/17.
+//  Copyright © 2017 Vagner Cazarotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
