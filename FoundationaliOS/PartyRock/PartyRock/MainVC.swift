c40f4205bc6ce57389affd4b7921ad4337b2e9fc
vagnercazarotto <vagnercazarotto@gmail.com>
2017-03-12 21:21:43 -0300

    Designing the UI for Party Rock

---
+++ FoundationaliOS/PartyRock/PartyRock/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRock
+//
+//  Created by Vagner Cazarotto on 3/9/17.
+//  Copyright © 2017 Vagner Cazarotto. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
